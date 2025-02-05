ed7bb30f4661284ca943c733d6ead58cf1f023fc
Rayssa Guimarães <guimaraesr305@gmail.com>
2025-02-05 14:37:08 -0300

    updating repo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./index.css"
+import "./index.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
